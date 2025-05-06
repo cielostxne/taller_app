a0aa224fe281c3c6ba8feee5eebd6d848e9aae82
Dankapp22 <digidankapp@gmail.com>
2025-05-06 16:46:50 -0400

    se muestran los usuarios

--- app/views/listaUsuarios.view.php
+++ app/views/listaUsuarios.view.php
@@ -1,3 +1,21 @@
+<?php
+// Conexión directa a la base de datos
+try {
+    $conn = new PDO("mysql:host=sql301.infinityfree.com;dbname=if0_38896219_elfaro_db", "if0_38896219", "030525faro");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Consulta a la base de datos
+    $stmt = $conn->query("SELECT nombre, correo FROM usuarios");
+    $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    die("Error al conectar o consultar: " . $e->getMessage());
+}
+?>
+
+
+
+
 <section class="section">
     <div class="container">
         <h1 class="title">Usuarios Registrados</h1>
